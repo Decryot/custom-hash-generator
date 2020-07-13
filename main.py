000c598b9c53b2e85d061e292e2f0b56924ab7c6
Dmitri <56200546+Decryot@users.noreply.github.com>
2020-07-13 15:59:56 -0400

    added a decryption method and loading / saving

--- main.py
+++ main.py
@@ -1,35 +1,66 @@
 import random
+import re
+import time
 
 hash_letters = ["a","c","d","e","f","i","l","m","n","o","r","u"
                 ,"0","1","2","3","4","5","6","7","8","9"]
 
-letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
+letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ", ".","-",",","'"
           ,"0","1","2","3","4","5","6","7","8","9"]
 
 hash_test = []
 
-max_hash_letters = 10
-min_hash_letters = 2
+max_hash_letters = 6
+min_hash_letters = 3
 
 # creates a sequence of letters for each letter
 
+# decrypts custom hash
+def decrypt_word(hash_word):
+    counter = 0
+    word_letters = list(hash_word)
+    word = hash_word
+    split_words = []
+    decryptedword = ""
+    file = None
+    for i in range(len(word_letters)):
+        try:
+            file = open("hash.txt", "r")
+
+            for hash_code in file:
+                if (word.startswith(hash_code.strip())):
+                    word = word.replace(hash_code.strip(),"",1)
+                    decryptedword += letters[counter]
+                counter+=1
+            counter = 0
+
+        except:
+            print("file not found")
+    return decryptedword
+
 def encrypt_word(word):
+
     word_letters = list(word)
     encrypted_word = ""
 
-    for i in word_letters:
-        for j in range(len(letters)):
-            if str.lower(i) == letters[j]:
-                encrypted_word += hash_test[j]
+    for wordletter in word_letters:
+        for letter_id in range(len(letters)):
+            if str.lower(wordletter) == letters[letter_id]:
+                encrypted_word += hash_test[letter_id].strip()
 
     print(encrypted_word)
+
+def load_hash():
+    file = open("hash.txt", "r")
+    for hash_code in file:
+        hash_test.append(hash_code)
 
 def save_hash():
     file = open("hash.txt","w+")
     for hash_code in hash_test:
         file.write(hash_code + "\n")
 
-def create_hash():
+def create_hash(can_save_hash):
     global letters
 
     for letter in range(len(letters)):
@@ -40,10 +71,14 @@ def create_hash():
             letter_id = random.randint(0,len(hash_letters)-1)
             new_hash += hash_letters[letter_id]
 
-        hash_test.append(new_hash)
-        print(new_hash + " " + letters[letter])
+        if can_save_hash:
+            hash_test.append(new_hash)
 
-    save_hash()
-    encrypt_word("hello")
+    #load_hash()
+    #save_hash()
+    #encrypt_word("hello")
 
-create_hash()
+    word = decrypt_word("cf8frf9nmcf8")
+    print(word)
+
+create_hash(False)
